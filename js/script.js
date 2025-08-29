e4e933b702564e9916cc522b7dbe87d086fa6933
injamul007 <injamul007hoque@gmail.com>
2025-08-29 20:18:38 +0600

    add and update click functionality to count heart in navbar

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
 //? Assignment 5 - Emergency Hotline all js code
 
-// function to capture all the id
+//? function to capture all the id
 
 function getElement(id) {
   const element = document.getElementById(id);
   return element;
 }
 
+//? addEventListener to the emergency hotline container
+
+getElement('emergency_hotline_container').addEventListener('click', function(e) {
+  if(e.target.className.includes('card_heart_icon')) {
+    const heartCount = getElement('nav_heart_count').innerText;
+    const currentHeartCount = Number(heartCount) + 1;
+    getElement('nav_heart_count').innerText = currentHeartCount;
+  }
+})
