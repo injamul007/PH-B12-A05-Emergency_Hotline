df360dd3f03309a94196f82b1ee1725ca31edeba
injamul007 <injamul007hoque@gmail.com>
2025-08-29 23:30:41 +0600

    update and remove some extra spaces from js file

--- js/script.js
+++ js/script.js
@@ -54,9 +54,10 @@ getElement('emergency_hotline_container').addEventListener('click', function(e) 
           </div>`;
 
           callHistoryShowDiv.append(newDivElement);
+          
   } else if(e.target.className.includes('clear_history_btn')) {
     getElement('call_history_show_div').innerText = '';
-    
+
   } else if(e.target.className.includes('copy_btn')) {
     const EmergencyCopyBtn = e.target;
 
@@ -72,7 +73,4 @@ getElement('emergency_hotline_container').addEventListener('click', function(e) 
     
     getElement('copy_count').innerText = currentCopyCount;
   }
-
-
-
-})
+});
