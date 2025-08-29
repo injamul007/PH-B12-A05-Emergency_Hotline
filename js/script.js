d17dd09983cc312576bf363da228fccde2a6e5b9
injamul007 <injamul007hoque@gmail.com>
2025-08-29 19:58:03 +0600

    add a function to capture all the id

--- js/script.js
+++ js/script.js
@@ -1,2 +1,9 @@
 //? Assignment 5 - Emergency Hotline all js code
 
+// function to capture all the id
+
+function getElement(id) {
+  const element = document.getElementById(id);
+  return element;
+}
+
